649e49b4ff766874800d23d0c1099c2696cf3826
Adrian Cable <adrian@starlinghome.io>
2026-01-06 11:08:36 -0800

    Add syscall fixup to ELF loader

--- arch/subleq/include/uapi/asm/unistd.h
+++ arch/subleq/include/uapi/asm/unistd.h
@@ -6,6 +6,16 @@
 #ifndef _UAPI_ASM_SUBLEQ_UNISTD_H
 #define _UAPI_ASM_SUBLEQ_UNISTD_H
 
+/* Enable new stat syscalls (fstat, fstatat, etc.) */
+#define __ARCH_WANT_NEW_STAT
+#define __ARCH_WANT_STAT64
+
+/* Enable 32-bit time syscalls (ppoll, pselect6, etc.) */
+#define __ARCH_WANT_TIME32_SYSCALLS
+
+/* Enable legacy syscalls that are superseded but still useful */
+#define __ARCH_WANT_RENAMEAT
+
 /* Use the generic system call table */
 #include <asm-generic/unistd.h>
 
